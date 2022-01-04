4c7fd0364d97a07f993e328e59f7f5defacb8c6b
Ashe Delanoix <adelanoix1@gmail.com>
2022-01-04 14:07:12 -0800

    better

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AsheDelanoix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hi, Enter your name party person!:");
+            var teacherName = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {teacherName} ready to party?!?!");
+        }
+    }
+}
+
